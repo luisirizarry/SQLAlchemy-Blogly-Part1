e78f6ab9241ffd6ef5662e2c0f1a8f7ad8203680
Luis Irizarry <lirizarry6178@gmail.com>
2024-09-26 11:26:36 -0400

    Finished tests

--- test_flask.py
+++ test_flask.py
@@ -4,7 +4,7 @@ from app import app
 from models import db, User
 
 # Use test database and don't clutter tests with SQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
 app.config['SQLALCHEMY_ECHO'] = False
 
 # Make Flask errors be real errors, rather than HTML pages with error info
@@ -16,13 +16,52 @@ app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 db.drop_all()
 db.create_all()
 
-class PetViewsTestCase(TestCase):
-    """Tests for views for Pets."""
+
+class UserViewsTestCase(TestCase):
+    """Tests for views for Users."""
 
     def setUp(self):
         """Add sample pet."""
+
+        User.query.delete()
+
+        user = User(first_name='Luis', last_name='Irizarry')
+        db.session.add(user)
+        db.session.commit()
+
+        self.user_id = user.id
+        self.user = user
 
     def tearDown(self):
         """Clean up any fouled transaction."""
 
         db.session.rollback()
+
+    def test_list_users(self):
+        """Test users list page."""
+        with app.test_client() as client:
+            resp = client.get("/users")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Luis Irizarry', html)
+
+    def test_show_user(self):
+        """Test show user page."""
+        with app.test_client() as client:
+            resp = client.get(f"/users/{self.user_id}")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Luis Irizarry', html)
+            
+
+    def test_add_user(self):
+        """Test add user page."""
+        with app.test_client() as client:
+            d = {"firstName": "John", "lastName": "Doe", "imageURL": ""}
+            resp = client.post("/users/new", data=d, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('John Doe', html)
